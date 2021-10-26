c809ef66e2d9b67b82f6e7cf039bf9b6c20991df
Samuel Ogbonnaya <75688944+FJSam@users.noreply.github.com>
2021-10-26 17:40:22 +0100

    Create on-start-sub-2.sh

---
+++ on-start-sub-2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+sudo -u ec2-user -i <<'EOF'
+unset SUDO_UID
+
+WORKING_DIR=/home/ec2-user/SageMaker/custom-miniconda-2
+
+# Create a custom conda environment
+source "$WORKING_DIR/miniconda/bin/activate"
+echo ". /home/ec2-user/SageMaker/custom-miniconda-2/etc/profile.d/conda.sh" >> ~/.bashrc
+
+cd /home/ec2-user/SageMaker/v2/xview3-reference
+echo "Creating xview3 conda environment"
+conda env create -f environment_v2.yml
+
+echo "Activating xview3 conda environment"
+source "$WORKING_DIR/miniconda/bin/activate" xview3_v2
+
+echo "Installing ipykernal and settng up jupyter kernel in background process."
+pip install ipykernel
+pip install --quiet boto3
+pip install sagemaker
+pip install efficientnet_pytorch
+pip install tensorboardX
+pip install pycocotools
+echo "Finished background installations"
+EOF
